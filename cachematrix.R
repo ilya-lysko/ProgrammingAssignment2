1a8c144b45405dc4b03f9dbfc58ee93ab96f9322
ilya-lysko <ilya-lysko@yandex-team.ru>
2018-08-10 16:22:29 +0300

    Add some comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,10 @@
+# This function create special structure (list) with following arguments:
+# s: cached matrix inversion (result of solve function)
+# set: function to set matrix
+# get: function to get matrix
+# setsolve: function to set matrix inversion
+# getsolve: function to get matrix inversion
+
 makeCacheMatrix <- function(x = matrix()) {
     s <- NULL
     set <- function(y) {
@@ -12,7 +19,11 @@ makeCacheMatrix <- function(x = matrix()) {
          getsolve = getsolve)
 }
 
-
+# This function calculates the inversion of matrix.
+# Firstly, it checks if inversion already was calculated.
+# If yes: return data from cache
+# else: calculate inversion, write it to special structure, created with the above function
+# and return it.
 cacheSolve <- function(x, ...) {
     s <- x$getsolve()
     if (!is.null(s)) {
